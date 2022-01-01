d3823359810e5d0cee91409b2152b67a5490341b
SaarChaffee <1240148332@qq.com>
2022-01-01 12:12:08 +0800

    params built in

--- src/reptile.js
+++ src/reptile.js
@@ -7,13 +7,18 @@ const getNews = (param, pageSize = 1) => {
   let pageIndex = 0;
   request(
     {
-      url: param.url,
       method: "GET",
-      headers: param.headers,
+      url: `https://ff.web.sdo.com/inc/newdata.ashx?url=List?gameCode=ff&category=5309,5310,5311,5312,5313&pageIndex=${pageIndex}&pageSize=${pageSize}`,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+        Accept: "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        Connection: "keep-alive",
+      },
     },
     (error, response, result) => {
       let data = JSON.parse(result).Data;
-
       data.forEach((item) => {
         datas.push({
           id: item.Id,
@@ -28,16 +33,5 @@ const getNews = (param, pageSize = 1) => {
   );
   return datas;
 };
-
-getNews({
-  url: `https://ff.web.sdo.com/inc/newdata.ashx?url=List?gameCode=ff&category=5309,5310,5311,5312,5313&pageIndex=${pageIndex}&pageSize=${pageSize}`,
-  headers: {
-    "User-Agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
-    Accept: "*/*",
-    "Accept-Encoding": "gzip, deflate, br",
-    Connection: "keep-alive",
-  },
-});
 
 module.exports = { getNews };
