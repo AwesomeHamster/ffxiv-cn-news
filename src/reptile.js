e5b3f04bfbac68d3c33cb4cd0fb1f26033cffe45
SaarChaffee <1240148332@qq.com>
2022-01-01 15:21:47 +0800

    extract params

--- src/reptile.js
+++ src/reptile.js
@@ -3,12 +3,19 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 const fs = require("fs");
 
-const getNews = async (pageSize = 1) => {
+let categorys = new Map([
+  ["newscont", "5309,5310,5311,5312,5313"],
+  ["", ""],
+]);
+
+const getNews = async (pageSize = 1, category = "newscont") => {
   let datas = new Array();
   let pageIndex = 0;
   let result = await axios({
     method: "GET",
-    url: `https://ff.web.sdo.com/inc/newdata.ashx?url=List?gameCode=ff&category=5309,5310,5311,5312,5313&pageIndex=${pageIndex}&pageSize=${pageSize}`,
+    url: `https://ff.web.sdo.com/inc/newdata.ashx?url=List?gameCode=ff&category=${categorys.get(
+      category
+    )}&pageIndex=${pageIndex}&pageSize=${pageSize}`,
     headers: {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
