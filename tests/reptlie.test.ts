fe1afb6e0a893e551a118d3f099bb906f3f24261
SaarChaffee <1240148332@qq.com>
2022-04-25 15:59:13 +0800

    add ts testing env

---
+++ tests/reptlie.test.ts
@@ -0,0 +1,10 @@
+import { describe ,it} from 'mocha';
+
+import { getNews } from "../src/index";
+
+describe("#reptile.js", () => {
+    it("test", async () => {
+        const arr = await getNews(0,5, "announcement");
+        console.log(arr);
+      });
+} )
