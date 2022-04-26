487c623dd0a440cd6424eff6c757647f537a9bf1
SaarChaffee <1240148332@qq.com>
2022-04-26 22:37:04 +0800

    base test

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,91 @@
-import  axios  from "axios";
+import axios from 'axios'
+import * as fs from 'fs-extra'
 
 export const categorys = new Map([
-    ["all-news", "5309,5310,5311,5312,5313"],
-    ["topics", "7186"],
-    ["news", "5310"],
-    ["official-events", "5311"],
-    ["announcement", "5312"],
-    ["unofficial-events", "5313"],
-]);
+  ['all-news', '5309,5310,5311,5312,5313'],
+  ['topics', '7186'],
+  ['news', '5310'],
+  ['official-events', '5311'],
+  ['announcement', '5312'],
+  ['unofficial-events', '5313'],
+])
 
-export async function getNews(
-    pageIndex:number = 0,
-    pageSize: number = 1,
-    category:string = "all-news"
-){
-    let datas = new Array()
-    let result = await axios({
-        method: "GET",
-        url: `https://ff.web.sdo.com/inc/newdata.ashx?url=List?gameCode=ff&category=${categorys.get(
-      category
-    )}&pageIndex=${pageIndex}&pageSize=${pageSize}`,
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
-      Accept: "*/*",
-      "Accept-Encoding": "gzip, deflate, br",
-      Connection: "keep-alive",
-    },
-    }).catch((error)=>{
-        console.log(error);
-    })
-    let data = result.data.Data
-    data.forEach(item => {
-        datas.push({
-            id: item.Id,
-            title: item.Title,
-            url: item.Author,
-            time: Date.parse(item.PublishDate),
-            image: item.HomeImagePath,
-            description: item.Summary,
-        })
-    });
-    return datas;
+export interface ResData {
+  Code: string
+  Message: string
+  PageCount: number
+  TotalCount: number
+  RecordCount: number
+  Data: DataArr[]
 }
+
+export interface DataArr {
+    Id: number
+    Articletype: number
+    ApplicationCode: number
+    CategoryCode: number
+    SortIndex: number
+    GroupIndex: number
+    TitleClass: string
+    Title: string
+    Author: string
+    PublishDate: Date
+    HomeImagePath: string
+    Summary: string
+    OutLink: string
+}
+
+export async function getNews(pageIndex: number = 0, pageSize: number = 1, category: string = 'all-news') {
+  let datas = new Array()
+  let headers = {
+    'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+  }
+  let url: string = `https://ff.web.sdo.com/inc/newdata.ashx?url=List?gameCode=ff&category=${categorys.get(
+    category,
+  )}&pageIndex=${pageIndex}&pageSize=${pageSize}`
+  await axios
+    .get<ResData>(url, {
+      headers,
+    })
+      .then((resp) => {
+          resp.data.Data.forEach(item => {
+            datas.push({
+                id: item.Id,
+                title: item.Title,
+                url: item.Author,
+                time: item.PublishDate,
+                image: item.HomeImagePath,
+                description: item.Summary,
+            })
+        })
+    })
+
+  return datas
+}
+// response.data.Data.forEach(item => {
+    // datas.push({
+    //     id: item.Id,
+    //     title: item.Title,
+    //     url: item.Author,
+    //     time: Date.parse(item.PublishDate),
+    //     image: item.HomeImagePath,
+    //     description: item.Summary,
+    // })
+// });
+// {
+//     method: "GET",
+//     url: `https://ff.web.sdo.com/inc/newdata.ashx?url=List?gameCode=ff&category=${categorys.get(
+//   category
+// )}&pageIndex=${pageIndex}&pageSize=${pageSize}`,
+// headers: {
+//   "User-Agent":
+//     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+//   Accept: "*/*",
+//   "Accept-Encoding": "gzip, deflate, br",
+//   Connection: "keep-alive",
+// },
+// }
--- tests/reptlie.test.ts
+++ tests/reptlie.test.ts
@@ -1,10 +1,10 @@
-import { describe ,it} from 'mocha';
+import { describe, it } from 'mocha'
+import { getNews } from '../src/index'
 
-import { getNews } from "../src/index";
-
-describe("#reptile.js", () => {
-    it("test", async () => {
-        const arr = await getNews(0,5, "announcement");
-        console.log(arr);
-      });
-} )
+describe('#reptile.js', () => {
+  it('test', async () => {
+    const arr = await getNews(0, 5, 'announcement')
+    console.log(arr)
+    // fs.writeFileSync("./res.json",arr)
+  })
+})
