d0278385508095f166d0f067a5dfb734578bdda4
SaarChaffee <1240148332@qq.com>
2022-04-26 23:55:30 +0800

    optimize request speed

--- src/index.ts
+++ src/index.ts
@@ -39,10 +39,11 @@ export async function getNews(pageIndex: number = 0, pageSize: number = 1, categ
   let datas = new Array()
   let headers = {
     'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
     'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Connection': 'keep-alive',
+    'Accept-Encoding': 'gzip, compress, br',
+    'Connection': 'close',
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
   }
   let url: string = `https://ff.web.sdo.com/inc/newdata.ashx?url=List?gameCode=ff&category=${categorys.get(
     category,
