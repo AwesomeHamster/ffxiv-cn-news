4642a1eb2c3f726973a5ee4015cb3e394b1d9d78
SaarChaffee <1240148332@qq.com>
2022-04-28 22:33:07 +0800

    add error handing

--- src/index.ts
+++ src/index.ts
@@ -20,19 +20,19 @@ export interface ResData {
 }
 
 export interface DataArr {
-    Id: number
-    Articletype: number
-    ApplicationCode: number
-    CategoryCode: number
-    SortIndex: number
-    GroupIndex: number
-    TitleClass: string
-    Title: string
-    Author: string
-    PublishDate: Date
-    HomeImagePath: string
-    Summary: string
-    OutLink: string
+  Id: number
+  Articletype: number
+  ApplicationCode: number
+  CategoryCode: number
+  SortIndex: number
+  GroupIndex: number
+  TitleClass: string
+  Title: string
+  Author: string
+  PublishDate: Date
+  HomeImagePath: string
+  Summary: string
+  OutLink: string
 }
 
 export async function getNews(pageIndex: number = 0, pageSize: number = 1, category: string = 'all-news') {
@@ -45,48 +45,41 @@ export async function getNews(pageIndex: number = 0, pageSize: number = 1, categ
     'Connection': 'close',
     'Cache-Control': 'no-cache, no-store, must-revalidate',
   }
+  let timeout: number = 1000
+  let responseEncoding: string = 'utf8'
   let url: string = `https://ff.web.sdo.com/inc/newdata.ashx?url=List?gameCode=ff&category=${categorys.get(
     category,
   )}&pageIndex=${pageIndex}&pageSize=${pageSize}`
-  await axios
-    .get<ResData>(url, {
+  let urlt = "http://aaa.aa"
+  let validateStatus = (status:number)=>(status>=200&&status<400)
+  if () {
+    
+  } else {
+    await axios
+    .get<ResData>(urlt, {
       headers,
+      timeout,
+      validateStatus,
     })
-      .then((resp) => {
-          resp.data.Data.forEach(item => {
-            datas.push({
-                id: item.Id,
-                title: item.Title,
-                url: item.Author,
-                time: item.PublishDate,
-                image: item.HomeImagePath,
-                description: item.Summary,
-            })
+    .then((resp) => {
+      resp.data.Data.forEach((item) => {
+        datas.push({
+          id: item.Id,
+          title: item.Title,
+          url: item.Author,
+          time: item.PublishDate,
+          image: item.HomeImagePath,
+          description: item.Summary,
         })
+      })
+    }).catch((err) => {
+      let error = err.toJSON()
+      datas.push({
+        error: error.code,
+        message: error.message
+      })
     })
-
+  }
   return datas
 }
-// response.data.Data.forEach(item => {
-    // datas.push({
-    //     id: item.Id,
-    //     title: item.Title,
-    //     url: item.Author,
-    //     time: Date.parse(item.PublishDate),
-    //     image: item.HomeImagePath,
-    //     description: item.Summary,
-    // })
-// });
-// {
-//     method: "GET",
-//     url: `https://ff.web.sdo.com/inc/newdata.ashx?url=List?gameCode=ff&category=${categorys.get(
-//   category
-// )}&pageIndex=${pageIndex}&pageSize=${pageSize}`,
-// headers: {
-//   "User-Agent":
-//     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
-//   Accept: "*/*",
-//   "Accept-Encoding": "gzip, deflate, br",
-//   Connection: "keep-alive",
-// },
-// }
+
