43eef8df165be67b973664920c8c61f6ed3b9ea0
SaarChaffee <1240148332@qq.com>
2022-04-30 00:11:44 +0800

    抽取配置

--- src/config.ts
+++ src/config.ts
@@ -1,33 +1,47 @@
 export const categorys = new Map([
-    ['all-news', '5309,5310,5311,5312,5313'],
-    ['topics', '7186'],
-    ['news', '5310'],
-    ['official-events', '5311'],
-    ['announcement', '5312'],
-    ['unofficial-events', '5313'],
-  ])
-  
-  export interface RespData {
-    Code: string
-    Message: string
-    PageCount: number
-    TotalCount: number
-    RecordCount: number
-    Data: DataArr[]
-  }
-  
-  export interface DataArr {
-    Id: number
-    Articletype: number
-    ApplicationCode: number
-    CategoryCode: number
-    SortIndex: number
-    GroupIndex: number
-    TitleClass: string
-    Title: string
-    Author: string
-    PublishDate: Date
-    HomeImagePath: string
-    Summary: string
-    OutLink: string
-  }
+  ['all-news', '5309,5310,5311,5312,5313'],
+  ['topics', '7186'],
+  ['news', '5310'],
+  ['official-events', '5311'],
+  ['announcement', '5312'],
+  ['unofficial-events', '5313'],
+])
+
+export interface RespData {
+  Code: string
+  Message: string
+  PageCount: number
+  TotalCount: number
+  RecordCount: number
+  Data: DataArr[]
+}
+
+export interface DataArr {
+  Id: number
+  Articletype: number
+  ApplicationCode: number
+  CategoryCode: number
+  SortIndex: number
+  GroupIndex: number
+  TitleClass: string
+  Title: string
+  Author: string
+  PublishDate: Date
+  HomeImagePath: string
+  Summary: string
+  OutLink: string
+}
+
+export const AxiosConfig = {
+  headers: {
+    'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, compress, br',
+    'Connection': 'close',
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
+  },
+  timeout: 1000,
+  responseEncoding: 'utf8',
+  validateStatus: (status: number) => status >= 200 && status < 400,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,46 @@
 import axios from 'axios'
-import * as fs from 'fs-extra'
-import {categorys,RespData,DataArr} from './config'
+import { categorys, RespData, DataArr, AxiosConfig } from './config'
 
 export async function getNews(pageIndex: number = 0, pageSize: number = 1, category: string = 'all-news') {
   let datas = new Array()
-  let headers = {
-    'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, compress, br',
-    'Connection': 'close',
-    'Cache-Control': 'no-cache, no-store, must-revalidate',
-  }
-  let timeout: number = 1000
-  let responseEncoding: string = 'utf8'
-  let url: string = `https://ff.web.sdo.com/inc/newdata.ashx?url=List?gameCode=ff&category=${categorys.get(
-    category,
-  )}&pageIndex=${pageIndex}&pageSize=${pageSize}`
-  let urlt = "http://aaa.aa"
-  let validateStatus = (status:number)=>(status>=200&&status<400)
-  if (pageIndex<0||pageSize<=0) {
+  let url: string =
+    'https://ff.web.sdo.com/inc/newdata.ashx?url=List?' +
+    'gameCode=ff' +
+    '&category=' +
+    categorys.get(category) +
+    '&pageIndex=' +
+    pageIndex +
+    '&pageSize=' +
+    pageSize
+  if (pageIndex < 0 || pageSize <= 0 || !categorys.has(category)) {
     datas.push({
-      error: "EPARAM",
-      message: "illegal parameters"
+      error: 'EPARAM',
+      message: 'illegal parameters',
     })
   } else {
+    let config = AxiosConfig
     await axios
-    .get<RespData>(url, {
-      headers,
-      timeout,
-      validateStatus,
-    })
-    .then((resp) => {
-      resp.data.Data.forEach((item) => {
-        datas.push({
-          id: item.Id,
-          title: item.Title,
-          url: item.Author,
-          time: item.PublishDate,
-          image: item.HomeImagePath,
-          description: item.Summary,
+      .get<RespData>(url, config)
+      .then((resp) => {
+        resp.data.Data.forEach((item) => {
+          datas.push({
+            id: item.Id,
+            title: item.Title,
+            url: item.Author,
+            time: item.PublishDate,
+            image: item.HomeImagePath,
+            description: item.Summary,
+          })
         })
       })
-    }).catch((err) => {
-      let error = err.toJSON()
-      datas.push({
-        error: error.code,
-        message: error.message
+      .catch((err) => {
+        console.log(err)
+        let error = err.toJSON()
+        datas.push({
+          error: error.code,
+          message: error.message,
+        })
       })
-    })
   }
   return datas
 }
-
