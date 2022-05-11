15aa6bd54a637d39d1f060964211a28319365195
Maiko Tan <maiko.tan.coding@gmail.com>
2022-05-11 12:53:47 +0000

    test: complete test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,9 +3,10 @@ import { getNews } from '../src'
 import { expect } from 'chai'
 
 
-describe('ffxiv cn news crawler', () => {
-  it('test', async () => {
+describe('#ffxiv cn news crawler', () => {
+  it('should fetch news', async () => {
     const arr = await getNews({})
     expect(arr).to.be.an('array').and.has.length.at.least(1)
+    expect(arr[0]).has.a.property('url').that.is.a('string')
   })
 })
